5850776180ff585be514cb141f70c9caac965036
senhorN <juniornicolas529@gmail.com>
2024-07-19 01:01:32 -0300

    database

---
+++ dbmemory.js
@@ -0,0 +1,27 @@
+import { randomUUID } from 'node:crypto'
+
+export class dbMemory{
+
+    #videos = new Map()
+
+    list(){
+        return  Array.from(this.#videos.values())
+    }
+
+    create(video){ 
+        
+        const videoId = randomUUID()
+
+        this.#videos.set(videoId, video)
+    }
+
+    update(id, video){
+
+        this.#videos.update(id, video)
+    }
+
+    delete(id){
+        
+        this.#videos.delete(id)
+    }
+}
